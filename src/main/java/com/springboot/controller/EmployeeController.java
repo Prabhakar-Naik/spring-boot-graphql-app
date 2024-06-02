06f9e7c9b24c4e38164f246ad7ff0d96c1e58b77
Prabhakar Naik K <prabhakarnaik225@gmail.com>
2024-06-02 10:29:11 +0530

    mongodb data added

--- src/main/java/com/springboot/controller/EmployeeController.java
+++ src/main/java/com/springboot/controller/EmployeeController.java
@@ -1,11 +1,23 @@
 package com.springboot.controller;
 
+import com.springboot.model.Employee;
+import com.springboot.model.Player;
+import com.springboot.model.Status;
+import com.springboot.model.Team;
 import com.springboot.service.EmployeeService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.annotation.SessionStatusMethodArgumentResolver;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/employees")
 public class EmployeeController {
 
     private final EmployeeService service;
@@ -14,5 +26,30 @@ public class EmployeeController {
         this.service = employeeService;
     }
 
+
+    @QueryMapping
+    public List<Employee> getEmployees(){
+        return this.service.getEmployees();
+    }
+
+    @QueryMapping
+    public Optional<Employee> getEmployee(@Argument @PathVariable String id){
+        return this.service.getEmployee(id);
+    }
+
+    @MutationMapping
+    public Employee createEmployee(@Argument String name, @Argument String email, @Argument double salary, @Argument Status status){
+        return this.service.createEmployee(new Employee("",name,email,salary,status));
+    }
+
+    @MutationMapping
+    public Employee updateEmployee(@Argument String id, @Argument String name, @Argument String email, @Argument double salary, @Argument Status status){
+        return this.service.updateEmployee(new Employee(id,name,email,salary,status));
+    }
+
+    @MutationMapping
+    public String deleteEmployee(@Argument String id){
+        return this.service.deleteEmployee(id);
+    }
 
 }
--- src/main/java/com/springboot/service/EmployeeService.java
+++ src/main/java/com/springboot/service/EmployeeService.java
@@ -1,35 +1,59 @@
 package com.springboot.service;
 
 import com.springboot.model.Employee;
+import com.springboot.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import javax.swing.text.html.Option;
+import java.util.*;
 
 @Service
 public class EmployeeService {
 
+    private final EmployeeRepository repository;
+
+    public EmployeeService(EmployeeRepository employeeRepository){
+        this.repository = employeeRepository;
+    }
+
 
     public List<Employee> getEmployees(){
-        return null;
+        List<Employee> employees = this.repository.findAll();
+        if (employees.isEmpty()){
+            return Collections.emptyList();
+        }
+        return employees.stream().toList();
     }
 
     public Optional<Employee> getEmployee(String id){
-        return null;
+        return this.repository.findById(id);
     }
 
     public Employee createEmployee(Employee employee){
-        return null;
+        employee.setId(UUID.randomUUID().toString());
+        this.repository.save(employee);
+        return employee;
     }
 
     public Employee updateEmployee(Employee employee){
+        Optional<Employee> optional = this.repository.findById(employee.getId());
+        if (optional.isPresent()){
+            optional.get().setName(employee.getName());
+            optional.get().setEmail(employee.getEmail());
+            optional.get().setSalary(employee.getSalary());
+            optional.get().setStatus(employee.getStatus());
+            this.repository.save(optional.get());
+            return optional.get();
+        }
         return null;
     }
 
     public String deleteEmployee(String id){
-        return null;
+        if (this.repository.findById(id).isPresent()){
+            this.repository.deleteById(id);
+            return "Deleted";
+        }
+        return "Not Deleted";
     }
 
 
