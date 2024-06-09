5594f7d2081a5e13a8f492715da7ee3572dae372
Prabhakar Naik <prabhakarnaik225@gmail.com>
2024-06-09 19:00:16 +0530

    Update PlayerRepository.java

--- src/main/java/com/springboot/repository/PlayerRepository.java
+++ src/main/java/com/springboot/repository/PlayerRepository.java
@@ -1,9 +1,9 @@
-package com.springboot.repository;
+// package com.springboot.repository;
 
-import com.springboot.model.Player;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+// import com.springboot.model.Player;
+// import org.springframework.data.mongodb.repository.MongoRepository;
+// import org.springframework.stereotype.Repository;
 
-@Repository
-public interface PlayerRepository extends MongoRepository<Player, Integer> {
-}
+// @Repository
+// public interface PlayerRepository extends MongoRepository<Player, Integer> {
+// }
